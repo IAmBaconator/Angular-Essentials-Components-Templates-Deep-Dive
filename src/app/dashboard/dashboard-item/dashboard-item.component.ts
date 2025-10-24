cde4baf56d9c249d6e7585ccd3bfe78bcbf0595e
IAmBaconator <107735348+IAmBaconator@users.noreply.github.com>
2025-10-24 19:50:28 +0000

    More CSS :host use allowing us to remove unnecessary div wrappers and letting angular do the wrapping.

--- src/app/dashboard/dashboard-item/dashboard-item.component.ts
+++ src/app/dashboard/dashboard-item/dashboard-item.component.ts
@@ -5,7 +5,10 @@ import { Component, Input } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './dashboard-item.component.html',
-  styleUrl: './dashboard-item.component.css'
+  styleUrl: './dashboard-item.component.css',
+  //host: {
+  //  class: 'dashboard-item'
+  //} //though this option works, it may not work to your advantage since this is looking for the element.class you removed from the template and this only extends to this component without including the encaspulation.none.
 })
 export class DashboardItemComponent {
   @Input({required: true}) image!: { src: string; alt: string; };
