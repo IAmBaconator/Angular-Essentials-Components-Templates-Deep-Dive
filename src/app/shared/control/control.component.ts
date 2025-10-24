f25a5cccfe4bd1997ab54f906664695813f398f3
IAmBaconator <107735348+IAmBaconator@users.noreply.github.com>
2025-10-24 19:34:52 +0000

    Different way to use CSS host classes

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -6,7 +6,10 @@ import { Component, input, Input, ViewEncapsulation } from '@angular/core';
   imports: [],
   templateUrl: './control.component.html',
   styleUrl: './control.component.css',
-  encapsulation: ViewEncapsulation.None //To disable the component encapsulation allowing elements outside this component to still get the css details.
+  encapsulation: ViewEncapsulation.None, //To disable the component encapsulation allowing elements outside this component to still get the css details.
+  host: {
+    class: 'control' //adds this as a property to your component selector (host element).
+  }
 })
 export class ControlComponent {
   @Input({required: true}) label!: string;
