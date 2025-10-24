a3b3eac6d161a75835d95219cae3860572d4d656
IAmBaconator <107735348+IAmBaconator@users.noreply.github.com>
2025-10-24 20:03:44 +0000

    Host alerternatives and host listening introduction.

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -8,9 +8,16 @@ import { Component, input, Input, ViewEncapsulation } from '@angular/core';
   styleUrl: './control.component.css',
   encapsulation: ViewEncapsulation.None, //To disable the component encapsulation allowing elements outside this component to still get the css details.
   host: {
-    class: 'control' //adds this as a property to your component selector (host element).
+    class: 'control', //adds this as a property to your component selector (host element).
+    '(click)': 'onClick()' //recommended option for listening to component elements.
   }
 })
 export class ControlComponent {
+  // @HostBinding('class') className = 'control'; //Alternative option instead of host: {class} above, but this is a legacy option.
+  // @HostListener() //Alternative option instead of host: {'(click)'} above, but this is a legacy option.
   @Input({required: true}) label!: string;
+
+  onClick() {
+    console.log('Clicked!');
+  }
 }
