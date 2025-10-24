7920b5c778a69c5bd7f9ea9ca36d63fbfa3051b0
IAmBaconator <107735348+IAmBaconator@users.noreply.github.com>
2025-10-24 19:23:41 +0000

    Migrating CSS to their respective components as well as making minor adjustments for shared components to work properly.

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,11 +1,12 @@
-import { Component, input, Input } from '@angular/core';
+import { Component, input, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
   standalone: true,
   imports: [],
   templateUrl: './control.component.html',
-  styleUrl: './control.component.css'
+  styleUrl: './control.component.css',
+  encapsulation: ViewEncapsulation.None //To disable the component encapsulation allowing elements outside this component to still get the css details.
 })
 export class ControlComponent {
   @Input({required: true}) label!: string;
