09f721f1fb1d36fdb963a79e5763e060d457e212
IAmBaconator <107735348+IAmBaconator@users.noreply.github.com>
2025-10-28 18:20:30 +0000

    Added more lifestyle methods.

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-server-status',
@@ -7,13 +8,15 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css'
 })
-export class ServerStatusComponent implements OnInit { // protect code by implementing intefaces to ensure propery spelling of lifestyle methods.
+export class ServerStatusComponent implements OnInit, OnDestroy{ // protect code by implementing intefaces to ensure propery spelling of lifestyle methods.
   currentStatus: 'online' | 'offline' | 'unknown' = 'offline';
+  private interval?: ReturnType<typeof setInterval>;
 
   constructor() {} // should really only reserve the constructor() for initializing classes and other small things to keep it clean.
   // Ref: Constructor Lifestyle https://angular.dev/guide/components/lifecycle
 
   ngOnInit() {
+    console.log('ON INIT');
     setInterval(() => {
       const rnd = Math.random(); // Will produce a value between 0 and 0.9999999999999
 
@@ -25,6 +28,15 @@ export class ServerStatusComponent implements OnInit { // protect code by implem
         this.currentStatus = 'unknown';
       }
     }, 5000);
+  }
+
+  ngAfterViewInit() {
+    console.log('AFTER VIEW INIT');
+  }
+
+  ngOnDestroy(): void {
+    clearTimeout(this.interval);
+    console.log('Interval Destroyed.'); // Clean up is good practice to battle against memory leaks.
   }
 
 }
