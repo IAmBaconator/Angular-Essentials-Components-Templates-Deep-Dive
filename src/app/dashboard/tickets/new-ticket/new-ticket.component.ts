4be17472526bd70b8c76ef8ed51c1182b9855459
IAmBaconator <107735348+IAmBaconator@users.noreply.github.com>
2025-11-03 23:42:44 +0000

    Adding back tw0-way binding.

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -14,6 +14,8 @@ export class NewTicketComponent implements OnInit, AfterViewInit{
   @ViewChild('form') form?: ElementRef<HTMLFormElement>; //Decorator to pull from template form.
   // private form = viewChild.required<ElementRef<HTMLFormElement>>('form'); //Available in A17.3, signal related feature that works like a decorator.
   //@Output() add = new EventEmitter(<{title: string; text: string;}>); //Legacy approach.
+  enteredTitle = ''; // non-signal option.
+  enteredText = '';
   add = output<{title: string; text: string;}>(); //Alternatvie more moder approach.
 
   ngOnInit(): void {
@@ -26,15 +28,17 @@ export class NewTicketComponent implements OnInit, AfterViewInit{
     console.log(this.form?.nativeElement);
   }
 
-  onSubmit(title: string, ticketText: string) {
+  onSubmit() {
     // console.dir(titleElement); //https://developer.mozilla.org/en-US/docs/Web/API/HTMLInputElement
     // const enteredTitle = titleElement.value;
     // console.log('ENTERED TITLE: ' + enteredTitle);
     // console.log(title);
     // console.log(ticketText);
-    this.add.emit({ title: title, text: ticketText });
+    this.add.emit({ title: this.enteredTitle, text: this.enteredText });
 
     //this.form?.nativeElement.reset();
-    this.form?.nativeElement.reset();
+    //this.form?.nativeElement.reset();
+    this.enteredTitle = '';
+    this.enteredText = '';
   }
 }
