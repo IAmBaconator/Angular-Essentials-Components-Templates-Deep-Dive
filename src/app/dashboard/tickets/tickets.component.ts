8ae3e4dfd0eeb0fe4edfc65d66c2e7037b51366a
IAmBaconator <107735348+IAmBaconator@users.noreply.github.com>
2025-11-04 00:04:27 +0000

    Added custom component in for custom two-way binding demo.

--- src/app/dashboard/tickets/tickets.component.ts
+++ src/app/dashboard/tickets/tickets.component.ts
@@ -1,17 +1,27 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { NewTicketComponent } from './new-ticket/new-ticket.component';
 import { Ticket } from './ticket/ticket.model';
 import { TicketComponent } from './ticket/ticket.component';
+import { RectComponent } from '../../shared/rect/rect.component';
+
 
 @Component({
   selector: 'app-tickets',
   standalone: true,
-  imports: [NewTicketComponent, TicketComponent],
+  imports: [NewTicketComponent, TicketComponent, RectComponent, FormsModule],
   templateUrl: './tickets.component.html',
   styleUrl: './tickets.component.css'
 })
 export class TicketsComponent {
+  // Custom Two-way Binding
+  rectSize = {
+    width: '100',
+    height: '100',
+  };
+
+  // Tickets Binding
   tickets: Ticket[] = [];
 
   onAdd(ticketData: { title: string; text: string; }) {
